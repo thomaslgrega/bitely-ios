3554d9d72e99cbc5c57610e48aa1618d25d85944
Thomas Grega <zeroyontlg@yahoo.com>
2025-04-21 17:45:16 -0700

    Initial Commit

---
+++ WhatWeEating/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatWeEating
+//
+//  Created by Thomas Grega on 4/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatWeEating/WhatWeEatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatWeEatingApp.swift
+//  WhatWeEating
+//
+//  Created by Thomas Grega on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatWeEatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
